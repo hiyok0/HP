f242cffb02c9597b94c68327c98365efc93fc248
hiyok0 <98054227+hiy0ko@users.noreply.github.com>
2023-11-19 06:52:04 +0900

    {{path}}が使えることに気がついたので修正

--- src/layouts/makeLink.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const moment = require('moment');
-module.exports = function makeLink(link, date, title){
-	return `/${((link === "page") ? "pages" : "blog/" + moment(date).format("YYYY"))}/${title.toLowerCase()}`;
-}
